72c410f312f4cc63c8cb0e74f1ecfc278517a140
kronk0s <bessaadkhalil@gmail.com>
2022-09-17 18:23:29 +0100

    simple showcase

--- src/components/Showcase.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useState } from "react";
-
-export default function Showcase() {
-  const [name, setName] = useState("John Doe");
-  return <div>Hello {name}</div>;
-}

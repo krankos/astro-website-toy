62973ad16963598b476dc842590a92dca416b83e
kronk0s <bessaadkhalil@gmail.com>
2022-12-21 10:40:18 +0100

    Landing page done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    fontFamily: {
-      sans: ["Poppins", "sans-serif"],
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
 };
